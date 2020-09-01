27b5a559c1169eb0a07e733efa567668dbbc83e5
bykim <byez02100@gmail.com>
2020-09-01 14:46:37 +0900

    first commit

---
+++ main.java
@@ -0,0 +1,6 @@
+public class Main {
+             puplic static void main(String [] args){
+                        System.out.println("Hello, World");
+                       //commit
+             }
+}
